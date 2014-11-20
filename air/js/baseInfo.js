6bdf8d20ee04f4133ea7cdf5bf6dab487c7fa709
Mr1024 <publiczhongemail@163.com>
2014-11-20 19:33:29 +0800

    Update

--- air/js/baseInfo.js
+++ air/js/baseInfo.js
@@ -1,1 +1,14 @@
-var baseInfo=function(){function t(){this.info=JSON.parse($.ajax({url:"/manifest.json",async:!1}).responseText)}var n;return{getInstance:function(){return n===undefined&&(n=new t),n}}}()
+var baseInfo = function() {
+    function t() {
+        this.info = JSON.parse($.ajax({
+            url: "/manifest.json",
+            async: !1
+        }).responseText)
+    }
+    var n;
+    return {
+        getInstance: function() {
+            return n === undefined && (n = new t), n
+        }
+    }
+}()
--- air/js/init.js
+++ air/js/init.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){(new Gallery).initShow("intro12306");setTimeout(function(){$("div.wrap").css("height","371px")},10)})
+$(document).ready(function() {
+    (new Gallery).initShow("intro12306");
+    setTimeout(function() {
+        $("div.wrap").css("height", "371px")
+    }, 10)
+})
